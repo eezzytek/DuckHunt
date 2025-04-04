c84941eb3ce9e20a0685835876295e631c40e2e9
Vladyslav Husenko <120654831+eezzytek@users.noreply.github.com>
2025-04-04 13:38:01 +0300

    Adding game constants

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ parser = argparse.ArgumentParser(description='Duck Hunt')
 parser.add_argument('--speed', type=float, default=1.0, help="Game speed factor (0.5 - faster, 1 - default, 2 - slower)")
 args = parser.parse_args()
 
+# Constants
+FPS = 60
+WIDTH, HEIGHT = 1366, 768
+GAME_TIME = 60
+RESULTS_FILE = 'scores.txt'
+COLORS = ['#F6FEAA', '#C7DFC5', '#C1DBE3']
+SPEED_MULTIPLIER = args.speed
+
